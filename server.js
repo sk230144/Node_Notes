384d8862f7a32c509bc871f51051c3961ee8037b
Saurabh Tiwari <risabht043@gmail.com>
2024-05-11 02:41:23 +0530

    11 May Node.js tut

--- server.js
+++ server.js
@@ -67,6 +67,7 @@ const cors = require("cors");
 const personRoute = require('./routes/personRputes') 
 const menuroutes = require('./routes/menuRotes');
 const passport = require('passport');
+const Person = require("./models/Persons");
 const LocalStrategy = require('passport-local').Strategy;
 
 
@@ -82,13 +83,67 @@ const logRequest = (req, res, next) => {
 
 app.use(logRequest)
 
-app.get('/', logRequest, (req, res) => {  // We will use middleware like this, this custom middleware tel us time when any url will hit.
-  res.send('Ka ho kaise aha');
-})
+
+//*****************************************Passsport Logic Implementation  **********************************************/
+
+passport.use(new LocalStrategy(async (username, password, done) => {
+  try {
+    console.log('Received Credentials:', username);
+    const user = await Person.findOne({ username });
+
+    if (!user) {
+      return done(null, false, { message: 'Incorrect Username' });
+    }
+
+    const isPasswordMatch = user.password === password;
+
+    if (isPasswordMatch) {
+      return done(null, user);
+    } else {
+      return done(null, false, { message: 'Incorrect Password' });
+    }
+  } catch (error) {
+    return done(error);
+  }
+}));
+
+
+app.use(passport.initialize());
+
+
+
+
+//**************************************************************All Done ********************************************************* */
+
+
+
+
+
 
 
 //Now call api
+
+//*****************************Calling that auth passport on person route without session for now */
+
+const localAuthMiddleware = passport.authenticate('local', {session: false});
+
+app.get('/', localAuthMiddleware, logRequest, (req, res) => {  // We will use middleware like this, this custom middleware tel us time when any url will hit.
+  res.send('Ka ho kaise aha');
+})
+
+// Now for this we have created a new user in person with username and opassword and for accessing this we ail pass that username and password like this in postman in params.......http://localhost:3000/?username=sakshi17&password=12345
+
+// ****** Now as like this for any api we can do this and he will see result after he have to pass username and password in params
+
+
+
 app.use('/person', personRoute);
+
+
+//// *****************************************Done Passport ****************************************
+
+
+
 app.use('/menu', menuroutes)
   
 
